db629dd8816a762164e5891028efb3d52b88ce88
Vasilev Alexander <vasilev@haulmont.com>
2022-05-31 20:50:11 +0400

    show declarative transaction

--- src/main/java/ru/trx/spring/data/demo/repository/CustomExamSheetRepositoryImpl.java
+++ src/main/java/ru/trx/spring/data/demo/repository/CustomExamSheetRepositoryImpl.java
@@ -1,33 +1,28 @@
 package ru.trx.spring.data.demo.repository;
 
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceUnit;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 /**
  * @author Alexander Vasiliev
  */
 public class CustomExamSheetRepositoryImpl implements CustomExamSheetRepository {
 
-    @PersistenceUnit
-    protected EntityManagerFactory entityManagerFactory;
+    @PersistenceContext
+    protected EntityManager em;
 
-    public CustomExamSheetRepositoryImpl(EntityManagerFactory entityManagerFactory) {
-        this.entityManagerFactory = entityManagerFactory;
+    public CustomExamSheetRepositoryImpl(EntityManager em) {
+        this.em = em;
     }
 
+    @Transactional
     @Override
     public void deleteStudent(Integer examSheetId, Integer studentId) {
-        var em = entityManagerFactory.createEntityManager();
-
-        em.getTransaction().begin();
-
         em.createNativeQuery("delete from exam_sheet_students es " +
                 "where es.exam_sheet_id = :examSheetId and es.students_id = :studentId")
                 .setParameter("studentId", studentId)
                 .setParameter("examSheetId", examSheetId)
                 .executeUpdate();
-
-        em.getTransaction().commit();
-        em.close();
     }
 }
