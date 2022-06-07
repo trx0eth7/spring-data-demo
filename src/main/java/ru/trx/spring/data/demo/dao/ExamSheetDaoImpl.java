a31339cb2e7b0b5611ffc2db74dc3de4bf67ab16
Vasilev Alexander <vasilev@haulmont.com>
2022-06-07 07:19:37 +0400

    impl dao using jpa

--- src/main/java/ru/trx/spring/data/demo/dao/ExamSheetDaoImpl.java
+++ src/main/java/ru/trx/spring/data/demo/dao/ExamSheetDaoImpl.java
@@ -1,10 +1,12 @@
 package ru.trx.spring.data.demo.dao;
 
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 import ru.trx.spring.data.demo.entity.ExamSheet;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceUnit;
 import java.util.List;
 
 /**
@@ -13,36 +15,46 @@ import java.util.List;
 @Repository
 public class ExamSheetDaoImpl implements ExamSheetDao {
 
-    protected SessionFactory sessionFactory;
+    @PersistenceUnit
+    protected EntityManagerFactory entityManagerFactory;
 
-    public ExamSheetDaoImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
+    public ExamSheetDaoImpl(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
     }
 
     @Override
     public void insert(ExamSheet examSheet) {
-        var tx = (Transaction) null;
+        var tx = (EntityTransaction) null;
+        var em = (EntityManager) null;
 
-        try (var session = sessionFactory.openSession()) {
-            tx = session.beginTransaction();
+        try {
+            em = entityManagerFactory.createEntityManager();
 
-            session.save(examSheet);
+            tx = em.getTransaction();
+            tx.begin();
 
+            em.persist(examSheet);
             tx.commit();
         } catch (Exception e) {
             if (tx != null) tx.rollback();
             throw e;
+        } finally {
+            if (em != null) em.close();
         }
     }
 
     @Override
     public List<ExamSheet> findAllByName(String name) {
-        var tx = (Transaction) null;
+        var tx = (EntityTransaction) null;
+        var em = (EntityManager) null;
 
-        try (var session = sessionFactory.openSession()) {
-            tx = session.beginTransaction();
+        try {
+            em = entityManagerFactory.createEntityManager();
 
-            var examSheets = session.createQuery("select e from ExamSheet e " +
+            tx = em.getTransaction();
+            tx.begin();
+
+            var examSheets = em.createQuery("select e from ExamSheet e " +
                             "where e.name = :name", ExamSheet.class)
                     .setParameter("name", name)
                     .getResultList();
@@ -53,6 +65,8 @@ public class ExamSheetDaoImpl implements ExamSheetDao {
         } catch (Exception e) {
             if (tx != null) tx.rollback();
             throw e;
+        } finally {
+            if (em != null) em.close();
         }
     }
 }
